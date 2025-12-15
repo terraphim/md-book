e07c18bc0f956d53992d46ea7ef8ee8dd0099883
AlexMikhalev <AlexMikhalev@users.noreply.github.com>
2025-12-15 15:36:38 +0000

    deploy: 01ef0653becba446b4f7c79c4a2f71c18a3b9f1d

--- docs/trait.impl/core/error/trait.Error.js
+++ docs/trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["md_book",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"md_book/pagefind_service/enum.PagefindError.html\" title=\"enum md_book::pagefind_service::PagefindError\">PagefindError</a>"]]]]);
+    var implementors = Object.fromEntries([["md_book",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"md_book/pagefind_service/enum.PagefindError.html\" title=\"enum md_book::pagefind_service::PagefindError\">PagefindError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- docs/trait.impl/core/marker/trait.Copy.js
+++ docs/trait.impl/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["md_book",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"md_book/config/enum.MarkdownFormat.html\" title=\"enum md_book::config::MarkdownFormat\">MarkdownFormat</a>"]]]]);
+    var implementors = Object.fromEntries([["md_book",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"md_book/config/enum.MarkdownFormat.html\" title=\"enum md_book::config::MarkdownFormat\">MarkdownFormat</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
